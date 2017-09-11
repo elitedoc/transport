9ea7fc8ba31fa578e42601eee3d33436d28fe755
mochoukr <mochoukr@ITEM-S15579.emea.msad.sopra>
2017-09-11 16:23:26 +0100

    11/09/2017

--- transport/src/main/java/com/choukrani/ui/client/ClientBean.java
+++ transport/src/main/java/com/choukrani/ui/client/ClientBean.java
@@ -1,9 +1,9 @@
 package com.choukrani.ui.client;
 
-import java.io.IOException;
+import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
@@ -12,111 +12,157 @@ import javax.faces.context.FacesContext;
 import org.primefaces.context.RequestContext;
 
 import com.choukrani.service.ClientService;
+import com.choukrani.model.Client;
 
-@ManagedBean
+@ManagedBean(name="clientBean")
 @ViewScoped
 public class ClientBean {
+	
+	//La liste des Blessures
+	private List<Client> listeClients ;
+	//Blessure DTO Objects
+	private Client client ;
+	//Boolean editForm Visible variable
+	private boolean clientEditFormVisible = false ;
+	
+	@ManagedProperty(value = "#{clientService}")
+	ClientService clientService;
+	public void init()
+	{
+		try{
+			client = new Client() ;
 
-	private Helper helper;
+		chargerClients() ; 
 
-	@ManagedProperty(value = "#{ClientService}")
-	private ClientService clientService;
-
-	private final static String ADD_SUCCESS = "Client ajouté avec succès";
-	private final static String ADD_ERROR = "Erreur, Client non ajouté";
-	private final static String DELETE_SUCCESS = "Client supprimé avec succès";
-	private final static String DELETE_ERROR = "Erreur, Client non supprimé";
-	private final static String UPDATE_SUCCESS = "Client modifié avec succès";
-	private final static String UPDATE_ERROR = "Erreur, Client non modifé";
-
-	public ClientBean() {
-		helper = new Helper();
-	}
-
-	public Helper getHelper() {
-		return helper;
-	}
-
-	public void setHelper(Helper helper) {
-		this.helper = helper;
-	}
-
-	public ClientService getClientService() {
-		return clientService;
-	}
-
-	public void setClientService(ClientService clientService) {
-		this.clientService = clientService;
-	}
-
-	// Chargement des données
-	@PostConstruct
-	public void init() {
-		this.getHelper().setListe(clientService.listerClients());
-	}
-
-	public void fetchToUpdate() {
-		this.helper.setClient(this.getClientService().recupererClient(this.getHelper().getSelectedId()));
-	}
-
-	// Suppression
-	public void delete() {
-		try {
-			// Supprimer
-			this.getClientService().supprimerClient(this.getHelper().getSelectedId());
-			// Recharger les données
-			init();
-			// Afficher le message de succès
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_INFO, DELETE_SUCCESS, DELETE_SUCCESS));
-		} catch (Exception e) {
-			e.printStackTrace();
-			// Afficher le message d'erreur
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_ERROR, DELETE_ERROR, DELETE_ERROR));
+		RequestContext.getCurrentInstance().update("idForm:idPanelClient");
 		}
-	}
-
-	// Création
-	public void add() {
-		try {
-			// Ajouter
-			this.getClientService().ajouterClient(this.helper.getClient());
-			// Recharger les données
-			init();
-			// Afficher le message de succès
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_INFO, ADD_SUCCESS, ADD_SUCCESS));
-			// Fermer la boite de dialogue
-			RequestContext.getCurrentInstance().execute("ajout.hide()");
-		} catch (Exception e) {
-			// Afficher le message d'erreur
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_ERROR, ADD_ERROR, ADD_ERROR));
+		catch(Exception ex)
+		{
+			addMessage(FacesMessage.SEVERITY_FATAL, "Un problème s'est produit lors du chargement des clients" );
 		}
+		
+	 }
+	
+	public void enregistrerAction()
+	{
+		enregistrerClient();
+		init() ;
+	}; 
+	
+	public String chargerClients()
+	{
+		listeClients = clientService.listerClients();
+		return null ;
 	}
-
-	// Modifier
-	public void update() {
-		try {
-			// Modifier
-			this.getClientService().modifierClient(this.helper.getClient());
-			// Recharger les données
-			init();
-			// Afficher le message de succès
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_INFO, UPDATE_SUCCESS, UPDATE_SUCCESS));
-			// Fermer la boite de dialogue
-			RequestContext.getCurrentInstance().execute("modification.hide()");
-		} catch (Exception e) {
-			// Afficher le message d'erreur
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_ERROR, UPDATE_ERROR, UPDATE_ERROR));
+	public String enregistrerClient()
+	{
+		try{
+		for(Client client : listeClients)
+			if(client.getClient_id() == null)
+				clientService.ajouterClient(client);
+			else
+				clientService.modifierClient(client);
 		}
+		catch(Exception ex)
+		{
+			addMessage(FacesMessage.SEVERITY_FATAL, "Un problème s'est produit lors de l'enregistrement des clients" );
+		}
+		return null ;
 	}
-
-	public void annuler() throws IOException {
-		FacesContext.getCurrentInstance().getExternalContext().redirect("client.xhtml");
+	// Ajouter une blessure
+	public String ajouterClientAction() {
+		try{
+		Client c = new Client();
+		c.setIntitule(client.getIntitule());
+		c.setAdresse(client.getAdresse());
+		listeClients.add(c);
+		client = new Client();
+		}
+		catch(Exception ex)
+		{
+			addMessage(FacesMessage.SEVERITY_FATAL, "Un problème s'est produit lors de l'ajout du client" );
+		}
+		return null;
+	}	
+	// Supprimer une blessure
+		public String supprimerClientAction(Client client) { 
+			try{
+			listeClients.remove(client);
+			if(client.getClient_id()!=null)
+				clientService.supprimerClient(client.getClient_id());
+			}
+			catch(Exception ex)
+			{
+				addMessage(FacesMessage.SEVERITY_FATAL, "Un problème s'est produit lors de la suppression de la blessure" );
+			}
+			return null;
 	}
+	// Modification d'un client
+		public String showClientEditFormAction(Client client) { 
+			try{
+			this.client = client ;
+			this.clientEditFormVisible = true ;
+			}
+			catch(Exception ex)
+			{
+				addMessage(FacesMessage.SEVERITY_FATAL, "Un problème s'est produit lors de l'affichage du client" );
+			}
+			return null;
+	}
+	public String updateClientAction() 
+	{ 
+		try{
+		this.clientEditFormVisible = false ;
+		client = new Client() ;
+		}
+		catch(Exception ex)
+		{
+			addMessage(FacesMessage.SEVERITY_FATAL, "Un problème s'est produit lors de la modification du client" );
+		}
+		return null;
+	}
+	// Cancel action
+		public String clientCancel() {
+		this.clientEditFormVisible = false ;
+		client = new Client(); 
+		return null;
+	}
+		
+		public List<Client> getListeClients() {
+			return listeClients;
+		}
 
+		public void setListeClients(List<Client> listeClients) {
+			this.listeClients = listeClients;
+		}
+
+		public Client getClient() {
+			return client;
+		}
+
+		public void setClient(Client client) {
+			this.client = client;
+		}
+
+		public boolean isClientEditFormVisible() {
+			return clientEditFormVisible;
+		}
+
+		public void setClientEditFormVisible(boolean clientEditFormVisible) {
+			this.clientEditFormVisible = clientEditFormVisible;
+		}
+
+		public ClientService getClientService() {
+			return clientService;
+		}
+
+		public void setClientService(ClientService clientService) {
+			this.clientService = clientService;
+		}
+
+		private void addMessage(Severity severity, String summary) {
+			FacesMessage message = new FacesMessage(severity, summary, null);
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+		
 }
